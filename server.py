60b813036d0d7cf26e84778d78af77e31aabe0e9
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-08-29 19:35:11 +0530

    I am an idiot

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def track_files():
 def delfiles(filep,db):
     table = db['files']
     if not table.find_one(filepath=filep):
-        print(content, file=sys.stderr)
+        print(filep, file=sys.stderr)
         os.remove(filep)
 
 @app.route('/v1/replytopull',methods = ['GET'])
